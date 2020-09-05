27d60711625fe44f2cbc90da82ec50d2e0955a8b
zato91 <zatokey@gmail.com>
2020-09-04 21:05:29 -0400

    testing form

--- src/App.js
+++ src/App.js
@@ -35,6 +35,7 @@ function App() {
     <Route path="/movie" component={Movie} />
 
    <SimpleModal />
+   {/* <SimpleRating /> */}
  
   </>
   );
--- src/SimpleRating.js
+++ src/SimpleRating.js
@@ -4,7 +4,7 @@ import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 
 export default function SimpleRating() {
-  const [value, setValue] = React.useState(10);
+  const [value, setValue] = React.useState(1);
 
   return (
     <div>
@@ -13,8 +13,16 @@ export default function SimpleRating() {
         <Typography component="legend">10 stars</Typography>
         <Rating name="size-large" defaultValue={2} size="large" value={value} max={10} />
       </Box>
+    {/* <Box component="fieldset" mb={3} borderColor="transparent">
+        <Typography component="legend">Controlled</Typography>
+        <Rating
+          name="simple-controlled"
+          value={value}
+          onChange={event => setValue(event.target.value)
+          }
+        />
+      </Box> */}
 
-      
       
     </div>
   );
